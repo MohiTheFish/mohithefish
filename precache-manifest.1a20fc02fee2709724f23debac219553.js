5ff8a41254e665fa1ea9b45c2b82b0362526e180
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-06-01 20:25:23 -0500

    Updates

---
+++ precache-manifest.1a20fc02fee2709724f23debac219553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c162737a24b2a0d09e74c13fb2920374",
+    "url": "/mohithefish/index.html"
+  },
+  {
+    "revision": "01aac98524c69e1709cc",
+    "url": "/mohithefish/static/css/main.300598c2.chunk.css"
+  },
+  {
+    "revision": "1e507574d7c7e1937d86",
+    "url": "/mohithefish/static/js/2.fa77d175.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mohithefish/static/js/2.fa77d175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01aac98524c69e1709cc",
+    "url": "/mohithefish/static/js/main.8a2a2c5f.chunk.js"
+  },
+  {
+    "revision": "7e980584c8107d96e38a",
+    "url": "/mohithefish/static/js/runtime-main.a7b87382.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/mohithefish/static/media/Raleway-Regular.9942588a.ttf"
+  }
+]);
