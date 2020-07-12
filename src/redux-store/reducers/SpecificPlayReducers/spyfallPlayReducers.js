65fcfc7916244f532cd5c4085ae05dd77366f8f0
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-07-12 11:20:46 -0500

    move spyfall reducers into separate file as well

---
+++ src/redux-store/reducers/SpecificPlayReducers/spyfallPlayReducers.js
@@ -0,0 +1,90 @@
+
+import {
+  HANDLE_LOCATION_SPYFALL,
+  ADD_NAME_SPYFALL,
+  REMOVE_NAME_SPYFALL,
+  START_GAME_SPYFALL,
+  UPDATE_MAIN_TIME,
+  CLEAR_SPYFALL_BOARD
+} from 'redux-store/actions/specificGameActions/spyfallActions';
+
+export const defaultSpyfallState = {
+  selectedLocations: new Map(),
+  selectedNamesByIndex: new Set(),
+  spyIndex: 0,
+  locations: [],
+  secretLocation: "",
+};
+
+
+const NUM_STATES_LOCATIONS = 3;
+
+
+export function spyfallReducers(state, action) {
+  switch(action.type) {
+    case START_GAME_SPYFALL: {
+      return {
+        ...state,
+        time: action.gameState.time,
+        spyfall: {
+          spyIndex: action.gameState.spyIndex,
+          selectedLocations: state.spyfall.selectedLocations,
+          selectedNamesByIndex: state.spyfall.selectedNamesByIndex,
+          locations: action.gameState.locations,
+          secretLocation: action.gameState.secretLocation,
+        },
+      };
+    }
+    case UPDATE_MAIN_TIME: {
+      return {
+        ...state,
+        time: action.time,
+      };
+    }
+    case ADD_NAME_SPYFALL: {
+      const {selectedNamesByIndex} = state.spyfall;
+      const clonedNames = new Set(selectedNamesByIndex);
+      clonedNames.add(action.data);
+      return Object.assign({}, state, {
+        spyfall: {
+          ...state.spyfall,
+          selectedNamesByIndex: clonedNames,
+        }
+      });
+    }
+    case REMOVE_NAME_SPYFALL: {
+      const {selectedNamesByIndex} = state.spyfall;
+      const clonedNames = new Set(selectedNamesByIndex);
+      clonedNames.delete(action.data);
+      return Object.assign({}, state, {
+        spyfall: {
+          ...state.spyfall,
+          selectedNamesByIndex: clonedNames,
+        }
+      });
+    }
+    case HANDLE_LOCATION_SPYFALL: {
+      const clonedLocations = new Map(state.spyfall.selectedLocations);
+      const oldVal = clonedLocations.get(action.data);
+      if(oldVal) {
+        clonedLocations.set(action.data, (oldVal+1) % NUM_STATES_LOCATIONS);
+      }
+      else {
+        clonedLocations.set(action.data, 1);
+      }
+      return Object.assign({}, state, {
+        spyfall: {
+          ...state.spyfall,
+          selectedLocations: clonedLocations,
+        }
+      })
+    }
+    case CLEAR_SPYFALL_BOARD: {
+      return Object.assign({}, state, {
+        spyfall: defaultSpyfallState,
+      });
+    }
+    default:
+      return state;
+  }
+}
