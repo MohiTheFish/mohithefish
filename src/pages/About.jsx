495750860005edc81be88d321819c7b6e52995b0
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-07 16:22:38 -0500

    Add option for details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,12 @@ import flightSimImg from 'assets/images/flightsim.png';
 
 import './About.scss';
 
-function LinkCard({ to, title, image, alt }) {
+function LinkCard({ to, title, image, alt, details }) {
   return (
     <Link to={to} className="link-card">
       <Paper className="paper">
         <h2>{title}</h2>
+        <p>{details}</p>
         <img src={image} alt={alt} />
       </Paper>
     </Link>
@@ -24,7 +25,13 @@ export default function AboutPage({ match }) {
     <div className="about-page">
       <h1>Projects</h1>
       <div className="projects-container">
-        <LinkCard to={`${base}/flight-sim`} title="Flight Simulator" image={flightSimImg} alt="A flight simulator game"/>
+        <LinkCard
+          to={`${base}/flight-sim`}
+          title="Flight Simulator"
+          image={flightSimImg}
+          alt="A flight simulator game"
+          details="This was an exercise in terrain generation and camera transformation as part of my computer graphics course at UIUC."
+        />
       </div>
     </div>
   )
