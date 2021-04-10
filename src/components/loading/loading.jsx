85f5dc97a3c613f468785553dd5bdd38bd34bdab
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-10 02:51:14 -0500

    Loading accepts an id prop

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './loading.scss';
 
-export default function Loading({id}) {
+export default function Loading({id, style}) {
   return (
-    <div id={id} className="lds-ripple">
+    <div id={id} className="lds-ripple" style={style}>
       <div />
       <div />
     </div>
