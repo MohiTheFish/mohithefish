989db4d7cf761b40af65a904e072e690e55110a6
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-16 12:51:23 -0500

    idk what this commit is for

--- src/Projects/ScreenTime/screenTime.jsx
+++ src/Projects/ScreenTime/screenTime.jsx
@@ -1,6 +1,79 @@
 import React, {useState} from 'react';
-import InputBox, {isInteger} from 'components/InputBox';
+import InputBox, {isInteger, InputDate} from 'components/InputBox';
 
+import './screenTime.scss';
+
+function isValidDay(day, month, year) {
+  const thirtyOneDayMonths = new Set([1,3,5,7,8,10,12]);
+  if (day < 1 || day > 31) {
+    return false;
+  }
+  if (day < 29) { // We don't need to do other checks
+    return true;
+  }
+  if (day <= 31 && thirtyOneDayMonths.has(day)) {
+    return true;
+  }
+  if (day <= 30 && month !== 2) {
+    return true;
+  }
+  if (day === 29 && month === 2 && (year % 4 === 0)) {
+    return true;
+  }
+  return false;
+}
+
+function checkDateValid(date) {
+  const {d: day, m: month, y: year} = date;
+  const errors = {
+    d: '',
+    m: '',
+    y: '',
+  };
+  let monthNum = 0;
+  let dayNum = 0;
+  let yearNum = 0;
+  
+  if (!isInteger(year)) {
+    errors.y = 'Year must be an integer';
+  }
+  else {
+    yearNum = Number.parseInt(year);
+    if (yearNum > 2021) {
+      errors.y = 'Must be a valid year';
+    }
+    else {
+      errors.y = '';
+    }
+  }
+
+  if(!isInteger(month)) {
+    errors.m = 'Month must be an integer';
+  }
+  else {
+    monthNum = Number.parseInt(month);
+    if (monthNum < 1 || monthNum > 12) {
+      errors.m = 'Month must be 1-12';
+    }
+    else {
+      errors.m = '';
+    }
+  }
+
+  if (!isInteger(day)) {
+    errors.d = 'Day must be an integer.';
+  }
+  else {
+    dayNum = Number.parseInt(day);
+    if (dayNum < 1 || !isValidDay(dayNum, monthNum, yearNum)) {
+      errors.d = 'Must be a valid day';
+    }
+    else {
+      errors.d = '';
+    }
+  }
+  return errors;
+}
 
 function checkHourValid(hour) {
   if(!isInteger(hour)) {
@@ -23,21 +96,72 @@ function checkMinuteValid(min) {
 
 
 export default function ScreenTime() {
-  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [date, setDay] = useState({
+    d: '2',
+    m: '2',
+    y: '2',
+  });
   const [hour, setHour] = useState("0");
   const [minute, setMinute] = useState("0");
 
+  const isDateValid = checkDateValid(date);
   const isHourValid = checkHourValid(hour);
   const isMinuteValid = checkMinuteValid(minute);
+
+  const handleDay = (e) => {
+    const {m,y} = date;
+    setDay({
+      d: e.value,
+      m,
+      y,
+    });
+  };
+  const handleMonth = (e) => {
+    const {d,y} = date;
+    setDay({
+      d,
+      m: e.value,
+      y,
+    });
+  }
+  const handleYear = (e) => {
+    const {m,d} = date;
+    setDay({
+      d,
+      m,
+      y: e.value,
+    });
+  }
+  const handleHour = (e) => {
+    setHour(e.value);
+  };
+  const handleMinute = (e) => {
+    setMinute(e.value);
+  };
+
+  console.log(isDateValid);
+  const {d,m,y} = date;
   return (
     <div className="screentime-wrapper">
       <h1>Some ScreenTime</h1>
-      <InputBox value={hour} setValue={setHour} errormsg={isHourValid} variant="outlined" className="time-entry">
-        <h3>Hour:</h3>
-      </InputBox>
-      <InputBox value={minute} setValue={setMinute} errormsg={isMinuteValid} variant="outlined" className="time-entry">
-        <h3>Minute:</h3>
-      </InputBox>
+      <div className="data-entry">
+        <h2 className="row-header">Data Point Entry</h2>
+        <input type="text" defaultValue={2} onChange={handleDay} className="date-entry"/>
+        <input type="text" defaultValue={2} onChange={handleMonth} className="date-entry"/>
+        <input type="text" defaultValue={2} onChange={handleYear} className="date-entry"/>
+        <input type="text" defaultValue={2} onChange={handleHour} className="time-entry"/>
+        <input type="text" defaultValue={2} onChange={handleMinute} className="time-entry"/>
+
+        {/* <InputDate value={date} setValue={setDate} errormsg={isDateValid} className="time-entry">
+          <h3>Date:</h3>
+        </InputDate>
+        <InputBox value={hour} setValue={setHour} errormsg={isHourValid} className="time-entry">
+          <h3>Hour:</h3>
+        </InputBox>
+        <InputBox value={minute} setValue={setMinute} errormsg={isMinuteValid} className="time-entry">
+          <h3>Minute:</h3>
+        </InputBox> */}
+      </div>
     </div>
   )
 }
--- src/components/InputBox/index.jsx
+++ src/components/InputBox/index.jsx
@@ -15,6 +15,61 @@ export function isFloat(arg) {
   return /^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/.test(arg);
 }
 
+export function InputDate(props) {
+  const { value, setValue, errormsg, children, styles, className, variant } = props;
+  const [month, day, year] = value;
+  // const question = children[0];
+  let error = Boolean(errormsg);
+
+  let inputProps = {
+    className: "time-input",
+    label: "Time",
+    onChange: handleChange, 
+  }
+  const InputType = variant === 'outlined' ? OutlinedInput : Input;
+
+  function handleChange(e) {
+    setValue(e.target.value.toUpperCase());
+  }
+  return (
+    <section className="setting">
+      {children}
+      <FormControl error={error} styles={styles} className={`full-width-input setting-date ${className ? className : ''}`}>
+        <InputType
+          fullWidth={true}
+          value={month}
+          {...inputProps}
+        />
+        {
+          error
+          ? <FormHelperText>{errormsg}</FormHelperText>
+          : null
+        }<span>/</span>
+        <InputType
+          fullWidth={true}
+          value={day}
+          {...inputProps}
+        />
+        {
+          error
+          ? <FormHelperText>{errormsg}</FormHelperText>
+          : null
+        }
+        <InputType
+          fullWidth={true}
+          value={year}
+          {...inputProps}
+        />
+        {
+          error
+          ? <FormHelperText>{errormsg}</FormHelperText>
+          : null
+        }
+      </FormControl>
+    </section>
+  );
+}
+
 export default function InputBox(props) {
   const { value, setValue, errormsg, children, styles, className, variant } = props;
   // const question = children[0];
