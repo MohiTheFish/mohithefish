70097ff07c7eeda8eb1905ae92cd040a2800c3ec
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-16 02:33:34 -0500

    error checking on times

--- src/Projects/ScreenTime/screenTime.jsx
+++ src/Projects/ScreenTime/screenTime.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InputBox, {isInteger} from 'components/InputBox';
+
+
+function checkHourValid(hour) {
+  if(!isInteger(hour)) {
+    return 'Hour must be a positive integer';
+  }
+  if (Number.parseInt(hour) >= 60) {
+    return 'Hour must be less than equal to 60.'
+  }
+  return '';
+}
+function checkMinuteValid(min) {
+  if(!isInteger(min)) {
+    return 'Minute must be a positive integer';
+  }
+  if (Number.parseInt(min) >= 60) {
+    return 'Minute must be less than equal to 60.'
+  }
+  return '';
+}
+
 
 export default function ScreenTime() {
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [hour, setHour] = useState("0");
+  const [minute, setMinute] = useState("0");
 
+  const isHourValid = checkHourValid(hour);
+  const isMinuteValid = checkMinuteValid(minute);
   return (
     <div className="screentime-wrapper">
       <h1>Some ScreenTime</h1>
+      <InputBox value={hour} setValue={setHour} errormsg={isHourValid} variant="outlined" className="time-entry">
+        <h3>Hour:</h3>
+      </InputBox>
+      <InputBox value={minute} setValue={setMinute} errormsg={isMinuteValid} variant="outlined" className="time-entry">
+        <h3>Minute:</h3>
+      </InputBox>
     </div>
   )
 }
--- src/components/InputBox/index.jsx
+++ src/components/InputBox/index.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 
 import './index.scss';
 
+export function isInteger(arg) {
+  return /^(\+|-)?\d+$/.test(arg);
+}
+
+export function isFloat(arg) {
+  return /^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/.test(arg);
+}
+
 export default function InputBox(props) {
-  const { value, setValue, errormsg, children, styles, className } = props;
+  const { value, setValue, errormsg, children, styles, className, variant } = props;
   // const question = children[0];
   let error = Boolean(errormsg);
 
@@ -17,6 +26,7 @@ export default function InputBox(props) {
     value: value,
     onChange: handleChange, 
   }
+  const InputType = variant === 'outlined' ? OutlinedInput : Input;
 
   function handleChange(e) {
     setValue(e.target.value.toUpperCase());
@@ -25,7 +35,7 @@ export default function InputBox(props) {
     <section className="setting">
       {children}
       <FormControl error={error} styles={styles} className={`full-width-input ${className ? className : ''}`}>
-        <Input
+        <InputType
           fullWidth={true}
           {...inputProps}
         />
