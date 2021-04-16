c690fe6e22d713c98e3ebe9f3bf755b5c3cf95a7
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-16 02:16:57 -0500

    consolidate padding style and new time endpoint

---
+++ src/Projects/ScreenTime/screenTime.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ScreenTime() {
+
+  return (
+    <div className="screentime-wrapper">
+      <h1>Some ScreenTime</h1>
+    </div>
+  )
+}
--- src/constants/projects.jsx
+++ src/constants/projects.jsx
@@ -6,8 +6,14 @@
 import FlightSimulator from 'pages/flightsim_temp';
 import AboutPage from 'pages/About';
 import StringAlignment from 'Projects/StringAlignment';
+import ScreenTime from 'Projects/ScreenTime/screenTime';
 
 import flightSimImg from 'assets/images/flightsim.png';
+
+
+const FLIGHTSIM_ENDPOINT = '/flight-sim';
+const STRINGALIGNMENT_ENDPOINT = '/string-alignment';
+const SCREENTIME_ENDPOINT = '/screentime';
 
 
 export const aboutRoutes = [
@@ -18,29 +24,41 @@ export const aboutRoutes = [
   },
   {
     exact: false,
-    path: '/flight-sim',
+    path: FLIGHTSIM_ENDPOINT,
     component: FlightSimulator,
   },
   {
     exact: false,
-    path: '/string-alignment',
+    path: STRINGALIGNMENT_ENDPOINT,
     component: StringAlignment,
+  },
+  {
+    exact: false,
+    path: SCREENTIME_ENDPOINT,
+    component: ScreenTime,
   }
 ];
 
 export const projectCards = [
   {
-    to: '/flight-sim',
+    to: FLIGHTSIM_ENDPOINT,
     title: "Flight Simulator",
     image: flightSimImg,
     alt: "A flight simulator game",
     details: "This was an exercise in terrain generation and camera transformation as part of my computer graphics course at UIUC."
   },
   {
-    to: '/string-alignment', 
+    to: STRINGALIGNMENT_ENDPOINT, 
     title: "String Alignment",
     image: undefined,
     alt: '',
     details: "This is a project to explain string alignment and align strings on demand."
+  },
+  {
+    to: SCREENTIME_ENDPOINT,
+    title: "Screen Time",
+    image: undefined,
+    alt: '',
+    details: "This is just a representation of how I waste my time regularly."
   }
 ];
