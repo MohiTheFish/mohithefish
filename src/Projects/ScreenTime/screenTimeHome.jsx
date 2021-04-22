a2e8cf293fc1e27fafafcfdb4beaf60c93dce5ac
Muhammed Imran <imranmuhammed2000@gmail.com>
2021-04-22 02:30:49 -0500

    add d3 mini script to the page

--- src/Projects/ScreenTime/screenTimeHome.jsx
+++ src/Projects/ScreenTime/screenTimeHome.jsx
@@ -6,13 +6,14 @@ import ScreenTimeVisualizor from './screenTimeVisualizor';
 import './screenTimeHome.scss';
 
 
+
 export default function ScreenTimeHome() {
 
-  const [editData, setEditData] = useState(true);
+  const [editData, setEditData] = useState(false);
 
   return (
-    <div className="screentime-wrapper">
-      <header>
+    <>
+      <header className="screentime-header">
         <h1>Screentime</h1>
         <Button color="primary" variant="contained" onClick={() => setEditData(!editData)}> {editData ? 'View Visualization' : 'Edit Data'}</Button>
       </header>
@@ -21,6 +22,6 @@ export default function ScreenTimeHome() {
         ? <ScreenTimeEditor />
         : <ScreenTimeVisualizor />
       }
-    </div>
+    </>
   )
 }
--- src/Projects/ScreenTime/screenTimeVisualizor.jsx
+++ src/Projects/ScreenTime/screenTimeVisualizor.jsx
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Loading from 'components/Loading/loading';
+
+import './screenTimeVisualizor.scss';
+
+const d3 = window.d3;
+
+const data = [{
+  "date": "03/30/2021",
+  "total": {
+    "hour": 8,
+    "minute": 56
+  },
+  "apps": [
+    {
+      "name": "Fire Emblem Heroes",
+      "hour": 4,
+      "minute": 48
+    },
+    {
+      "name": "Youtube",
+      "hour": 1,
+      "minute": 51
+    },
+    {
+      "name": "Netflix",
+      "hour": 1,
+      "minute": 8
+    },
+    {
+      "name": "Reddit",
+      "hour": 0,
+      "minute": 29
+    },
+    {
+      "name": "Messenger",
+      "hour": 0,
+      "minute": 11
+    },
+    {
+      "name": "Chrome",
+      "hour": 0,
+      "minute": 8
+    },
+    {
+      "name": "Gmail",
+      "hour": 0,
+      "minute": 3
+    },
+    {
+      "name": "Messages",
+      "hour": 0,
+      "minute": 1
+    },
+    {
+      "name": "Discord",
+      "hour": 0,
+      "minute": 1
+    }
+  ]
+}];
 
 export default function ScreenTimeVisualizor() {
+  let d3Div = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (d3Div) {
+      const svgCanvas = d3.select(d3Div)
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .style("border", "1px solid black")
+
+      svgCanvas.selectAll("rect")
+        .data(data).enter()
+          .append("rect")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .attr("fill", "orange");
+      setIsLoading(false);
+    }
+  }, [d3Div])
+
   return (
     <div className="screentime-visualizor">
-      See the pretty graphs.
+      {
+        isLoading
+        ? <Loading />
+        : null
+      }
+      <div id="d3div" className={isLoading ? 'hidden' : ''} ref={el => d3Div = el} />
     </div>
   )
 }
